75bff08a80fa1fac5690f603422e71d42f1b57f5
SaviourMeme <deemakn@yahoo.ca>
2023-02-14 16:20:39 -0500

    Added the View and finalized grabbing the image and text

--- src/main/java/com/example/lab5lh1203665/MobilePhone.java
+++ src/main/java/com/example/lab5lh1203665/MobilePhone.java
@@ -1,4 +1,26 @@
 package com.example.lab5lh1203665;
 
+import javafx.scene.image.Image;
+
 public class MobilePhone {
+
+    public String model = "Samsung";
+
+    public MobilePhone(String model) {
+        setModel(model);
+    }
+
+    private void setModel(String model) {
+        this.model = model;
+    }
+
+    /**
+     * This method return an Image object to represent the card
+     */
+
+    public Image getImage(){
+        String imageName = "images/" + model +".png";
+        return new Image(MobilePhone.class.getResourceAsStream(imageName));
+    }
+
 }
--- src/main/java/com/example/lab5lh1203665/MobilePhoneController.java
+++ src/main/java/com/example/lab5lh1203665/MobilePhoneController.java
@@ -1,4 +1,30 @@
 package com.example.lab5lh1203665;
 
-public class MobilePhoneController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MobilePhoneController implements Initializable {
+
+    @FXML
+    private Label model;
+
+    @FXML
+    private ImageView phone;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        phoneImage();
+
+    }
+
+    private void phoneImage(){
+        MobilePhone mobilePhone = new MobilePhone("Samsung");
+        phone.setImage(mobilePhone.getImage());
+        model.setText(mobilePhone.model);
+    }
 }
